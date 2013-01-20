c23e7110f4748d562448d5dc333c34cb6a3b2827
Jack Franklin <jack@jackfranklin.net>
2013-01-20 22:55:48 +0000

    fix add.js

--- app/modules/add.js
+++ app/modules/add.js
@@ -1,1 +1,8 @@
-define([],function(){var e={twoNumbers:function(e,t){return e+t}};return e});
+define([], function() {
+  var add = {
+    twoNumbers: function(x, y) {
+      return x+y;
+    }
+  };
+  return add;
+});
