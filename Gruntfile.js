dd5a2511f4b7d644ad0afb40b7754f16cfc42a49
Jack Franklin <jack@jackfranklin.net>
2013-01-22 18:56:33 +0000

    use grunt contrib-connect to set up a basic server for previewing the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,22 @@ module.exports = function(grunt) {
     },
     jshint: {
       all: ["app/**/*.js"]
+    },
+    connect: {
+      preview: {
+        options: {
+          keepalive: true,
+          port: 3000
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('build', ['requirejs']);
+  grunt.registerTask('server', ['connect:preview']);
 
 };
