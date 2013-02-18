9eea214e8d9614aacddf0807586621122cf665a7
David <dtang85@gmail.com>
2013-02-18 15:34:42 -0800

    Switches custom event system to use Ampfliy.js. From the documentation: It is possible to implement the publish and subscribe model by using jQuery custom events, however, the AmplifyJS pub/sub component provides a slightly cleaner interface, prevents collisions between custom events and method names, and allows a priority to your messages.

--- app/app.js
+++ app/app.js
@@ -7,23 +7,23 @@
 
     Utilities: {},
 
-    Events: $({}),
+    Events: amplify,
 
     init: function () {
 
       // here we are looping round all of the modules in our app.Modules object. We could have an exclude array for modules
       // that we don't want to be immediately initialised. We could initialise them later on in our application lifecycle
-
+      
       for( var x in App.Modules ) {
         App.Modules[x].init();
       }
 
       // the most useful part of this is Events. Modules shouldn't know about each other, so they're completely decoupled. We use
-      // app.Events to 'trigger' and use 'on' to send/receive messages and data around our application. The 'trigger' function
-      // takes data as it's second parameter which is accessible in the 'params' object in the receiving function, i.e. look at the
-      // 'render' function within the 'jesse' module
+      // app.Events to 'publish' and 'subscribe' to send/receive messages and data around our application. The 'publish' function
+      // takes data as parameters after the topic name. Data parameters are accessible as arguments in the subscribing function.
+      // See amplify.js documentation for PubSub.
 
-      App.Events.trigger( 'render' );
+      App.Events.publish( 'render' );
     }
   };
 
